484b6d1f7b0d53b26dec6c7ffc52532003cf530a
Maximiliano Príncipe <mlapeducacionit@gmail.com>
2025-10-30 10:49:42 -0300

    Trabajamos con el Service Worker y agregarmos el hero

--- sw.js
+++ sw.js
@@ -12,4 +12,35 @@ self.addEventListener('fetch', e => {
     console.log('sw fetch!')
 
     console.log(e.request) // Un objeto que representa una petición http
+    /* console.log(e.request.url); */
+    const { url, method } = e.request // desestructuring Object
+    console.warn('url:', url);
+    console.warn('method:', method);
+    //let respuesta = fetch(e.request.url) // Hace el request a url
+    let respuesta = fetch(e.request)
+
+
+    console.warn('Es un css?', url.includes('.css') ? 'Si' : 'No')
+
+    if (url.includes('style.css') ) {
+        // let respuesta = null
+        /* let respuesta = new Response(`
+            @import "tailwindcss";
+
+            .w-20 { width: 20%;}
+            .w-30 { width: 30%;}
+            .w-40 { width: 40%;}
+            .w-50 { width: 50%;}
+            .w-60 { width: 60%;}
+            .w-70 { width: 70%;}
+            .w-80 { width: 80%;}
+            .w-90 { width: 90%;}
+            .w-100 { width: 100%;}
+        `, { headers: { 'content-type': 'text/css' }})
+        e.respondWith(respuesta) */
+    } else {
+        e.respondWith(respuesta) // El SW se encarga de responder
+    }
+
+
 })
